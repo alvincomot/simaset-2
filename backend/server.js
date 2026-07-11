a05d3fbdb38e09806363eafd69f5bb7fb1c18a39
alvin <limithunter1@gmail.com>
2026-07-11 11:19:02 +0700

    feat: register asset management api routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from "./routes/auth/authRoutes.js";
+import assetRoutes from './routes/auth/assetRoute.js'
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/assets', assetRoutes);
 
 app.get('/', (req, res) => {
     res.json({message: 'Hello World!'});
