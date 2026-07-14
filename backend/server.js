31504b1bbb191f09392c85e643fe81674180bcb0
alvin <limithunter1@gmail.com>
2026-07-14 15:48:30 +0700

    feat: register master routes and borrowing routes in express application

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import authRoutes from "./routes/auth/authRoutes.js";
-import assetRoutes from './routes/auth/assetRoute.js'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import assetRoutes from "./routes/assetRoute.js";
+import masterRoutes from "./routes/masterRoute.js";
+import borrowingRoutes from "./routes/borrowingRoute.js";
 
 dotenv.config();
 
@@ -12,13 +14,15 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/assets', assetRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/assets", assetRoutes);
+app.use("/api/masters", masterRoutes);
+app.use("/api/borrowing", borrowingRoutes);
 
-app.get('/', (req, res) => {
-    res.json({message: 'Hello World!'});
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
