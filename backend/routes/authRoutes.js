cd65d4a8e4576bcaf036657ee5028b4144fe1a43
alvin <limithunter1@gmail.com>
2026-07-23 23:01:19 +0700

    feat: add auth routes for registration

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { login, forgotPassword, resetPassword } from "../controllers/authController.js";
+import { login, forgotPassword, resetPassword, register, verifyEmail } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post('/login', login);
+router.post('/register', register);
+router.get('/verify-email/:token', verifyEmail);
 router.post('/forgot-password', forgotPassword);
 router.post('/reset-password/:token', resetPassword);
 
