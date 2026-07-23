a495cb8d05a8773d8a3742853396ed0999b29a77
alvin <limithunter1@gmail.com>
2026-07-23 23:01:19 +0700

    feat: add borrowing reject route

--- backend/routes/borrowingRoute.js
+++ backend/routes/borrowingRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken, authorizeRoles } from '../middlewares/authMiddleware.js';
-import { requestBorrowing, approveBorrowing, returnAsset, getBorrowings } from '../controllers/borrowingController.js';
+import { requestBorrowing, approveBorrowing, rejectBorrowing, returnAsset, getBorrowings } from '../controllers/borrowingController.js';
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.get('/', verifyToken, getBorrowings);
 router.post('/request', verifyToken, authorizeRoles('USER'), requestBorrowing);
 //admion area
 router.post('/approve/:id', verifyToken, authorizeRoles('SUPER_ADMIN', 'STAFF'), approveBorrowing);
+router.post('/reject/:id', verifyToken, authorizeRoles('SUPER_ADMIN', 'STAFF'), rejectBorrowing);
 router.post('/return/:id', verifyToken, authorizeRoles('SUPER_ADMIN', 'STAFF'), returnAsset);
 
 export default router;
